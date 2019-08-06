5443cd535ef63313b4841ea6749eb8bd2b64d28b
zhiai <zhiai@qq.com>
2019-08-06 19:25:24 +0800

    登录静态布局完成

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,6 +1,6 @@
 <template>
   <footer class="footer_guide border-1px">
-    <a href="javascript:;" class="guide_item" :class="{on:$route.path==='/msite'}" @click="goTo('/miste')">
+    <a href="javascript:;" class="guide_item" :class="{on:$route.path==='/msite'}" @click="goTo('/msite')">
       <span class="item_icon">
         <i class="iconfont icon-waimai"></i>
       </span>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Header from 'components/Header/Header.vue'//注册全局头部组件
 
 Vue.config.productionTip = false
-
+Vue.component('Header',Header)//注册全局头部组件
 new Vue({
   //第一种方式：引入一个带编译器的文件在脚手架配置文件里面
   // components: {
