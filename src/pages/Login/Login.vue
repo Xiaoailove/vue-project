9a87480924f17e066c6a01080f7b41262472b97d
zhiai <zhiai@qq.com>
2019-08-07 21:08:53 +0800

    login 前台登录部分功能完成

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,16 +4,18 @@
       <div class="login_header">
         <h2 class="login_logo">硅谷外卖</h2>
         <div class="login_header_title">
-          <a href="javascript:;" class="on">短信登录</a>
-          <a href="javascript:;">密码登录</a>
+          <a href="javascript:;" :class="{on:loginWay}" @click="loginWay=true">短信登录</a>
+          <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
         </div>
       </div>
       <div class="login_content">
         <form>
-          <div class="on">
+          <div :class="{on:loginWay}">
             <section class="login_message">
-              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone" @click="clickA">
-              <button disabled="disabled" class="get_verification">获取验证码</button>
+              <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+              <button :disabled="!isRightPhone || computeTime>0" class="get_verification" 
+              :class={right_phone_number:isRightPhone} 
+              @click.prevent="sendCode">{{computeTime>0? `短信已发送(${computeTime}s)` :'获取验证码'}}</button>
             </section>
             <section class="login_verification">
               <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,16 +25,16 @@
               <a href="javascript:;">《用户服务协议》</a>
             </section>
           </div>
-          <div>
+          <div :class="{on:!loginWay}">
             <section>
               <section class="login_message">
                 <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
               </section>
               <section class="login_verification">
-                <input type="tel" maxlength="8" placeholder="密码">
-                <div class="switch_button off">
-                  <div class="switch_circle"></div>
-                  <span class="switch_text">...</span>
+                <input :type="isShowPwd?'text':'password'" maxlength="8" placeholder="密码">
+                <div class="switch_button" @click="isShowPwd=!isShowPwd" :class="isShowPwd?'on' : 'off'">
+                  <div class="switch_circle" :class="{right:isShowPwd}"></div>
+                  <span class="switch_text">{{isShowPwd?'abc':''}}</span>
                 </div>
               </section>
               <section class="login_message">
@@ -46,21 +48,42 @@
         <a href="javascript:;" class="about_us">关于我们</a>
       </div>
       <a href="javascript:" class="go_back">
-        <i class="iconfont icon-jiantou2"></i>
+        <i class="iconfont icon-jiantou2" @click="goBack('/profile')"></i>
       </a>
   </div>
   </section>
 </template>
 <script type="text/ecmascript-6">
+import { setInterval, clearInterval } from 'timers';
   export default {
     data () {
       return {
-          phone:''
+          loginWay:true,
+          phone:'',
+          computeTime:0,
+          isShowPwd:false
       }
     },
+    computed:{
+      isRightPhone () {
+        return /^1\d{10}$/.test(this.phone)
+      },
+
+    },
     methods:{
-      clickA () {
-        
+      sendCode () {
+        this.computeTime=10
+       const timerId=setInterval(()=>{
+         //clearInterval(timerId)
+          this.computeTime--
+          //当时间倒计时为0的时候自动停止定时器
+          if(this.computeTime===0){
+            clearInterval(timerId)
+          }
+        },1000)
+      },
+      goBack (path) {
+        this.$router.push(path)
       }
     }
   }
@@ -127,6 +150,8 @@
                 color #ccc
                 font-size 14px
                 background transparent
+                &.right_phone_number
+                  color black
             .login_verification
               position relative
               margin-top 16px
@@ -166,6 +191,9 @@
                   background #fff
                   box-shadow 0 2px 4px 0 rgba(0,0,0,.1)
                   transition transform .3s
+                  &.right
+                    transform translateX(27px)
+                    transition transform .3s
             .login_hint
               margin-top 12px
               color #999
